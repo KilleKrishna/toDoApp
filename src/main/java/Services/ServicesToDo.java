d9c5fecc9475c90e675e9d1d8052a01572c0f790
vvinod <vvinod@tavisca.in>
2019-09-10 10:33:16 +0530

    get methods written

--- src/main/java/Services/ServicesToDo.java
+++ src/main/java/Services/ServicesToDo.java
@@ -1,4 +1,28 @@
 package Services;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServicesToDo {
+    private List<String> todos = new ArrayList<>();
+
+    public ResponseEntity<?> getTodos() throws JSONException {
+        return sendResponse("All Todos returned");
+    }
+
+    public ResponseEntity<?> getTodoById(int todoid) throws JSONException {
+        if (todoid >= todos.size())
+            return new ResponseEntity<>("", HttpStatus.BAD_REQUEST);
+        JSONObject jsonResponse = new JSONObject()
+                .put("todoname", todos.get(todoid))
+                .put("status", "Todo " + (todoid + 1) + " retrieved")
+                .put("timestamp", Instant.now().toString());
+        return new ResponseEntity<>(jsonResponse.toString(), HttpStatus.OK);
+    }
 }
