7295077d2720f414fb14309d437f9f1850802c2d
vvinod <vvinod@tavisca.in>
2019-09-10 10:39:01 +0530

    other methods written

--- src/main/java/Services/ServicesToDo.java
+++ src/main/java/Services/ServicesToDo.java
@@ -25,4 +25,35 @@ public class ServicesToDo {
                 .put("timestamp", Instant.now().toString());
         return new ResponseEntity<>(jsonResponse.toString(), HttpStatus.OK);
     }
+
+    public ResponseEntity<?> addTodo(String json) throws JSONException {
+        JSONObject obj = new JSONObject(json);
+        String todoname = obj.getString("todoname");
+        todos.add(todoname);
+        return sendResponse("Todo " + todoname + " Added");
+    }
+
+    public ResponseEntity<?> updateTodo(int todoid, String json) throws JSONException {
+        JSONObject jsonObject = new JSONObject(json);
+        if (todoid >= todos.size())
+            return new ResponseEntity<>("", HttpStatus.BAD_REQUEST);
+        String todoname = jsonObject.getString("todoname");
+        todos.set(todoid, todoname);
+        return sendResponse("Todo-" + (todoid + 1) + " Updated");
+    }
+
+    public ResponseEntity<?> deleteTodo(int todoid) throws JSONException {
+        if (todoid >= todos.size())
+            return new ResponseEntity<>("", HttpStatus.BAD_REQUEST);
+        todos.remove(todoid);
+        return sendResponse("Todo-" + (todoid + 1) + " Deleted");
+    }
+
+    private ResponseEntity<?> sendResponse(String status) throws JSONException {
+        JSONObject jsonResponse = new JSONObject()
+                .put("todos", todos)
+                .put("status", status)
+                .put("timestamp", Instant.now().toString());
+        return new ResponseEntity<>(jsonResponse.toString(), HttpStatus.OK);
+    }
 }
