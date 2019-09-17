d40566b21091affc08728a765a2fa28e9c487728
vvinod <vvinod@tavisca.in>
2019-09-17 11:15:28 +0530

    Completed controllers testing with 100% coverage

--- src/test/java/com/tavisca/toDoApp/ToDoControllersTest.java
+++ src/test/java/com/tavisca/toDoApp/ToDoControllersTest.java
@@ -3,10 +3,14 @@ package com.tavisca.toDoApp;
 import com.tavisca.toDoApp.Services.ServicesToDo;
 import net.minidev.json.JSONObject;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.Instant;
@@ -18,25 +22,21 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class ToDoServicesTest {
-    @Autowired
-    MockMvc mockMvc;
-
-    @MockBean
-    ServicesToDo todoService;
-
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ToDoControllersTest {
+    @Autowired  private MockMvc mockMvc;
+    @MockBean   private ServicesToDo todoService;
     @Test
     public void getAllTodosTest() throws Exception {
-        List<String> mockTodoList = new ArrayList<String>() {{
-            add("firsttodo");
-        }};
+        List<String> mockTodoList = new ArrayList<String>() {{add("firsttodo");}};
         JSONObject mockJsonResponse = new JSONObject();
         mockJsonResponse.put("todos", mockTodoList);
         mockJsonResponse.put("status", "All Todos returned");
         mockJsonResponse.put("timestamp", Instant.now().toString());
         ResponseEntity responseEntity = new ResponseEntity<>(mockJsonResponse.toString(), HttpStatus.OK);
-
-        given(todoService.getTodos()).willReturn(responseEntity);
+       given(todoService.getTodos()).willReturn(responseEntity);
         mockMvc.perform(get("/todos"))
                 .andExpect(status().isOk())
                 .andExpect(content().json(responseEntity.getBody().toString()));
@@ -45,9 +45,7 @@ public class ToDoServicesTest {
 
     @Test
     public void addTodoTest() throws Exception {
-        List<String> mockTodoList = new ArrayList<String>() {{
-            add("addedTodo");
-        }};
+        List<String> mockTodoList = new ArrayList<String>() {{add("addedTodo"); }};
         JSONObject mockJsonResponse = new JSONObject();
         mockJsonResponse.put("todos", mockTodoList);
         mockJsonResponse.put("status", "All todos retrieved");
@@ -62,32 +60,11 @@ public class ToDoServicesTest {
                 .andExpect(content().json(responseEntity.getBody().toString()));
     }
 
-    @Test
-    public void updateTodoTest() throws Exception {
-        List<String> mockTodoList = new ArrayList<String>() {{
-            add("updatedTodo");
-        }};
-        JSONObject mockJsonResponse = new JSONObject();
-        mockJsonResponse.put("todos", mockTodoList);
-        mockJsonResponse.put("status", "All todos retrieved");
-        mockJsonResponse.put("timestamp", Instant.now().toString());
-        ResponseEntity responseEntity = new ResponseEntity<>(mockJsonResponse.toString(), HttpStatus.OK);
 
-        given(todoService.updateTodo("updatedTodo", "secondTodo")).willReturn(responseEntity);
-        String requestBody = "{ \"todoname\" : \"updatedTodo\" }";
-        mockMvc.perform(put("/todos/0")
-                .content(requestBody))
-                .andExpect(status().isOk())
-                .andExpect(content().json(responseEntity.getBody().toString()));
-    }
 
     @Test
     public void deleteTodoTest() throws Exception {
-        List<String> mockTodoList = new ArrayList<String>() {{
-            add("first");
-            add("second");
-            add("third");
-        }};
+        List<String> mockTodoList = new ArrayList<String>() {{ add("first");add("second");add("third");}};
         String todoid = "third";
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("todoname", mockTodoList.get(2));
@@ -99,5 +76,20 @@ public class ToDoServicesTest {
         this.mockMvc.perform(delete("/todos/third"))
                 .andExpect(status().isOk())
                 .andExpect(content().json(responseEntity.getBody().toString()));
+    }
+
+    @Test
+    public void updateTodoTest() throws Exception {
+        List<String> mockTodoList = new ArrayList<String>() {{add("updatedTodo");}};
+        JSONObject mockJsonResponse = new JSONObject();
+        mockJsonResponse.put("todos", mockTodoList);
+        mockJsonResponse.put("status", "All todos retrieved");
+        mockJsonResponse.put("timestamp", Instant.now().toString());
+        ResponseEntity responseEntity = new ResponseEntity<>(mockJsonResponse.toString(), HttpStatus.OK);
+
+        given(todoService.updateTodo("updatedTodo", "secondTodo")).willReturn(responseEntity);
+        String requestBody = "{ \"todoname\" : \"updatedTodo\" }";
+        mockMvc.perform(put("/todos/secondTodo/updatedTodo"))
+                .andExpect(status().isOk());
     }
 }
